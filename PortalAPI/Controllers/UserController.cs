d991a09109a673e8fc7bc32d09f599761b3d4bb6
ajgorinho10 <100563904+ajgorinho10@users.noreply.github.com>
2025-03-20 23:14:39 +0100

    Podstawa Api
    Brakuje tego czegoś do uwierzytelniania użytkownika odnośnie przyszłych zapytań

---
+++ PortalAPI/Controllers/UserController.cs
@@ -0,0 +1,101 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using PortalAPI.Data;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace PortalAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUsers()
+        {
+            var data = await _context.Users.ToListAsync();
+
+            return Ok(new ApiResponse<List<PortalAPI.Data.User>>(true, "", data));
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(int id)
+        {
+            var user = await _context.Users.FindAsync(id);
+            if (user == null) return Ok(new ApiResponse<User>(false, "Nie znaleziono użytkownika", null));
+            return Ok(new ApiResponse<User>(true, "Znaleziono użytkownika", user));
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<User>> CreateUser(User user)
+        {
+            try
+            {
+                var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Login == user.Login);
+                if (existingUser != null)
+                {
+                    return Ok(new ApiResponse<User>(false, "Podany Login już istnieje", null));
+                }
+
+                if(!PassCheck.isPassValid(user.Password))
+                {
+                    return Ok(new ApiResponse<User>(false, "Nie prawidłowe hasło", null));
+                }
+
+                if (!PassCheck.isLoginValid(user.Login))
+                {
+                    return Ok(new ApiResponse<User>(false, "Nie prawidłowy login", null));
+                }
+
+                user.Password = PassCheck.HashAndSalt(user.Password);
+
+                _context.Users.Add(user);
+                await _context.SaveChangesAsync();
+
+                return Ok(new ApiResponse<User>(true, "Stworzono użytkownika", user));
+            }
+            catch (DbUpdateException ex) {
+                if (ex.InnerException is SqlException sqlException &&
+            sqlException.Number == 2627)
+                {
+                    return Ok(new ApiResponse<User>(false, "Podany Login już istnieje", null));
+                }
+
+                return StatusCode(500, "An error occurred while processing your request.");
+            }   
+        }
+
+        [HttpPost("/[controller]/login")] 
+        public async Task<ActionResult<User>> loggin(User user)
+        {
+            var existingUser = await _context.Users.FirstOrDefaultAsync(u => u.Login == user.Login);
+            if (existingUser == null)
+            {
+                return Ok(new ApiResponse<User>(false, "Błędny login lub hasło", null));
+            }
+            else
+            {
+                if(PassCheck.VerifyPassword(user.Password, existingUser.Password))
+                {
+                    return Ok(new ApiResponse<User>(true, "Zalogowano", user));
+                }
+                else
+                {
+                    return Ok(new ApiResponse<User>(false, "Błędny login lub hasło", null));
+                }
+            }
+
+
+        }
+
+    }
+}
---
+++ PortalAPI/Data/ApiResponse.cs
@@ -0,0 +1,16 @@
+﻿namespace PortalAPI.Data
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; } // Czy operacja się powiodła?
+        public string Message { get; set; } // Wiadomość zwrotna
+        public T Data { get; set; } // Zwracane dane (mogą być dowolnym typem)
+
+        public ApiResponse(bool success, string message, T data = default)
+        {
+            Success = success;
+            Message = message;
+            Data = data;
+        }
+    }
+}
