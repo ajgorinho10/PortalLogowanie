a77464c0b6a7168b9eb77759e0e88badca2df269
ajgorinho10 <kowalskimarek0909@gmail.com>
2025-05-16 20:20:54 +0200

    Koniec

--- LogowanieAPI/LogowanieAPI/SecurityAndValidation/RegisterRequestValidator.cs
+++ LogowanieAPI/LogowanieAPI/SecurityAndValidation/RegisterRequestValidator.cs
@@ -10,11 +10,11 @@ namespace LogowanieAPI.SecurityAndValidation
         {
             RuleFor(x => x.FirstName)
                 .NotEmpty().WithMessage("Imię jest wymagane")
-                .MaximumLength(50);
+                .MaximumLength(50).WithMessage("Imię ma maksymalnie 50 znaków");
 
             RuleFor(x => x.LastName)
                 .NotEmpty().WithMessage("Nazwisko jest wymagane")
-                .MaximumLength(50);
+                .MaximumLength(50).WithMessage("Nazwisko ma maksymalnie 50 znaków");
 
             RuleFor(x => x.Login)
                 .NotEmpty().WithMessage("Login jest wymagany")
@@ -33,7 +33,7 @@ namespace LogowanieAPI.SecurityAndValidation
 
             RuleFor(x => x.Password)
                 .NotEmpty().WithMessage("Hasło jest wymagane")
-                .MinimumLength(8).WithMessage("Hasło musi mieć co najmniej 6 znaków")
+                .MinimumLength(12).WithMessage("Hasło musi mieć co najmniej 6 znaków")
                 .Matches("[A-Z]").WithMessage("Hasło musi zawierać dużą literę")
                 .Matches("[0-9]").WithMessage("Hasło musi zawierać cyfrę");
 
